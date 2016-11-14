a97eacd8d62c0de7cc410b5c4b0e8db04d090aa3
Willow <revolyw@yeah.net>
2016-11-14 09:45:07 +0800

    add rest controller

--- src/main/java/controller/RestfulController.java
+++ src/main/java/controller/RestfulController.java
@@ -1,8 +1,19 @@
 package controller;
 
+import model.Cipher;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Willow on 16/11/13.
  */
 @RestController
 public class RestfulController {
+    @RequestMapping("/getCipher")
+    public Cipher getCipher(@RequestParam(value = "hello word!") String plain) {
+        Cipher cipher = new Cipher();
+        cipher.setPlain(plain);
+        return cipher;
+    }
 }
